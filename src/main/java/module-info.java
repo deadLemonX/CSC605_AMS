93dd52eab3460cffea5b9ffa860101997da26d97
Matthew Rivera <mjrivera0613dev@gmail.com>
2023-09-12 20:37:26 -0700

    removed module

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.ams {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.ams to javafx.fxml;
-    exports com.example.ams;
-}

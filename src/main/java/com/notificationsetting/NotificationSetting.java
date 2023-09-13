daff8fb15561ebb5a9fc78a0018a8b24ee010bcb
Tekati <tekatiger28@gmail.com>
2023-09-12 20:54:51 -0400

    Notification Setting

--- src/main/java/com/notificationsetting/NotificationSetting.java
+++ src/main/java/com/notificationsetting/NotificationSetting.java
@@ -5,18 +5,15 @@ import com.twilio.type.PhoneNumber;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.io.UnsupportedEncodingException;
-import java.net.Authenticator;
-import java.net.PasswordAuthentication;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.Properties;
 import java.util.Random;
@@ -30,8 +27,6 @@ public class NotificationSetting {
     private static final String ACCOUNT_SID = "AC8e1f927b80c8ed7e7d7b40e2a0257be8";
     private static final String AUTH_TOKEN = "3c0e554c1f6db0579c473cfd21656922";
     private static final String PHONE_NUMBER = "18557751147";
-    private static Object secretKey;
-    private static Object key;
 
     public static void main(String[] args) {
         String databaseUrl = "jdbc:mysql://localhost:3306/mydatabase";
@@ -44,21 +39,19 @@ public class NotificationSetting {
         String userAppliance = String.valueOf(databaseManager.getCellValue("user_id", "appliances", userId, "user"));
         String userDevicesLocation = String.valueOf(databaseManager.getCellValue("user_id", "appliances_location", userId, "user"));
 
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);//Initialize the Twilo client
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);// Initialize the Twilio client
         boolean userApplianceStatus = getRandomStatus(); // Generate a random status
         String applianceStatus = monitorApplianceStatus(userAppliance, userDevicesLocation, userApplianceStatus);
         String encryptedMessage = encryptionMessage(applianceStatus, "AMS4everYAY");// Encrypt the message
         sendEncryptedEmail(userEmail, encryptedMessage, userApplianceStatus); // Send encrypted email notification
-        sendEncryptedSMS(userPhoneNumber, "Appliance Status Alert", encryptedMessage, userApplianceStatus); //Send encrypted email notification
-
+        sendEncryptedSMS(userPhoneNumber, "Appliance Status Alert", encryptedMessage, userApplianceStatus); // Send encrypted SMS notification
     }
 
-    //Random status(true for running, false for not working)
+    // Random status(true for running, false for not working)
     public static boolean getRandomStatus() {
         Random random = new Random();
         return random.nextBoolean();
     }
-
 
     // Monitor appliance status
     public static String monitorApplianceStatus(String appliance, String location, boolean status) {
@@ -72,9 +65,8 @@ public class NotificationSetting {
 
     private static String encrypt(final String strToEncrypt, final String secret) {
         try {
-            setKey(secret);
+            SecretKeySpec secretKey = setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            Key secretKey = null;
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
             return Base64.getEncoder()
                     .encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
@@ -84,30 +76,30 @@ public class NotificationSetting {
         }
     }
 
-    private static void setKey(final String myKey) {
+    private static SecretKeySpec setKey(final String myKey) {
         try {
             MessageDigest sha = MessageDigest.getInstance("SHA-256");
             byte[] keyBytes = myKey.getBytes(StandardCharsets.UTF_8);
             sha.update(keyBytes, 0, keyBytes.length);
             keyBytes = sha.digest();
-            secretKey = new SecretKeySpec(keyBytes, "AES");
-        } catch (NoSuchAlgorithmException e) {
+            return new SecretKeySpec(keyBytes, "AES");
+        } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
     }
 
-    //Alert Message
-    public static void alertMessage(String applianceName, boolean applianceStatus, String applianceLocation) {
-        String message = "Alert: " + applianceName + " in " + applianceLocation + " is " + (applianceStatus ? "running" : "not working");
-        sendAlertMessage(message);
+    // Alert Message
+    public static String alertMessage(String applianceName, boolean applianceStatus, String applianceLocation) {
+        return "Alert: " + applianceName + " in " + applianceLocation + " is " + (applianceStatus ? "running" : "not working");
     }
 
     public static void sendEncryptedEmail(String recipientEmail, String encryptedMessage, boolean status) {
-        if (!status) { //Appliance is not running
+        if (!status) { // Appliance is not running
             // Email configuration
             String fromEmail = EMAIL;
-            String smtpHost = "smtp.x.com"; // ams smtp server
-            int smtpPort = 587; // ams smtp port
+            String smtpHost = "smtp.x.com"; // AMS SMTP server
+            int smtpPort = 587; // AMS SMTP port
 
             // Setup properties for the email server
             Properties props = new Properties();
@@ -119,12 +111,12 @@ public class NotificationSetting {
             // Authenticate with the email server
             Authenticator a = new Authenticator() {
                 protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(fromEmail, EMAIL_PASSWORD.toCharArray());
+                    return new PasswordAuthentication(fromEmail, EMAIL_PASSWORD);
                 }
             };
 
             // Create a session with the email server
-            Session session = Session.getInstance(props);
+            Session session = Session.getInstance(props, a);
 
             try {
                 // Create MimeMessage
@@ -140,18 +132,18 @@ public class NotificationSetting {
                 e.printStackTrace();
             }
         } else {
-            System.out.print("Appliance is still in good health");
+            System.out.println("Appliance is still in good health");
         }
     }
 
     // Send an encrypted SMS
-// If statement for appliance status is false to send message
+    // If statement for appliance status is false to send a message
     public static void sendEncryptedSMS(String phoneNumber, String subject, String encryptedMessage, boolean status) {
         if (!status) {
             try {
                 com.twilio.rest.api.v2010.account.Message message = com.twilio.rest.api.v2010.account.Message.creator( // Fully qualify Message class
-                        new PhoneNumber(phoneNumber), // user phone number
-                        PhoneNumber(PHONE_NUMBER), // ams Twilio phone number
+                        new PhoneNumber(phoneNumber), // User phone number
+                        new PhoneNumber(PHONE_NUMBER), // AMS Twilio phone number
                         encryptedMessage // Message body
                 ).create();
 
